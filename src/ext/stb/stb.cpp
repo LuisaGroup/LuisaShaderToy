5fbf14dec480585c3d392a0bbdc8e2f4987d0171
mike <mango@live.cn>
2021-06-25 19:46:47 +0800

    good

---
+++ src/ext/stb/stb.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Mike Smith on 2021/6/13.
+//
+
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb/stb_image.h>
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include <stb/stb_image_write.h>
